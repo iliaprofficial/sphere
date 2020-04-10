2aec5c6f0eac0f8b16ae0cf2d56ee025e30d309f
iliaprofficial <iliapro@outlook.com>
2020-04-10 18:55:03 +0300

    json.js deleted

--- scripts/json.js
+++
@@ -1,7 +0,0 @@
-let getJson = async function(url){ // Get json
-	try {
-		const response = await fetch(url);
-		return await response.json();
-	} catch (err) {throw err}
-}
-module.exports = {getJson}
